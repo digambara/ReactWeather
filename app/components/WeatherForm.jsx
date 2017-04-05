b9c3e4838edf955fd7a701e403ffca749d50fbc5
siva <siva.karasala@gmail.com>
2017-04-05 11:34:50 -0400

    Style nav bar

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -15,7 +15,7 @@ var WeatherForm = React.createClass({
     return (
       <div>
         <form onSubmit={this.onFormSubmit}>
-          <input type="text" placeholder='Enter city name' ref="location"/><br/>
+          <input type="text" placeholder='Enter city name' ref="location"/>
           <button>Get Weather</button>
         </form>
       </div>
