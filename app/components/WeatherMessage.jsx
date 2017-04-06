f816be92b9b3b2de3747397f8a09df6a5b0795d2
siva <siva.karasala@gmail.com>
2017-04-06 08:11:12 -0400

    Add styles to weather page

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -12,7 +12,7 @@ var React = require('react');
 // Refactoring : Stateless Functional Components
 var WeatherMessage = ({temp, location}) => {
   return (
-    <h3>It's {temp} in {location}.</h3>
+    <h3 className="text-center">It's {temp} in {location}.</h3>
   )
 };
 
